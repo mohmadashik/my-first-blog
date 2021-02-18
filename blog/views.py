835307f898d5c63ba2336643db36f1fffcc0a11b
mohmadashik <ashikg8coder@gmail.com>
2021-02-18 16:45:51 +0530

    Added new Posts and removed old posts.Extended application

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.utils import timezone
 # Create your views here.
@@ -6,3 +6,6 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.order_by('published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
+def post_detail(request,pk):
+    posts=get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':posts})
